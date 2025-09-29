9569c00f58b476a2b0a944dee17cf3c2a61e4612
CipherAtlas <sabargulati777@gmail.com>
2025-09-29 22:53:45 +0530

    THE GAME IS NOW COMPLETE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/parcel-panic/' : '/',
+  base: command === 'build' ? './' : '/',
   server: {
     open: true,
   },
